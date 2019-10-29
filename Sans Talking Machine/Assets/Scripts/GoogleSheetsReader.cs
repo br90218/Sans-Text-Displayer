bc36a70929915fefe082b416ca723696674c26c0
Brian Teng <askmeifyouwantmyusername@gmail.com>
2019-10-28 20:58:09 -0400

    finished?

---
+++ Sans Talking Machine/Assets/Scripts/GoogleSheetsReader.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using GoogleSheetsToUnity;
+
+
+public class GoogleSheetsReader : MonoBehaviour
+{
+
+    public string spreadSheetId, tabName;
+    public TextParser TextParser;
+
+    private float timer;
+    private string _currentDialogueText;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _currentDialogueText = string.Empty;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer > 3f)
+        {
+            GetDialogue();
+            timer = 0;
+        }
+
+        timer += Time.deltaTime;
+    }
+
+    private void GetDialogue()
+    {
+        SpreadsheetManager.ReadPublicSpreadsheet(new GSTU_Search(spreadSheetId, tabName), UpdateDialogue);
+    }
+
+    private void UpdateDialogue(GstuSpreadSheet callBack)
+    {
+        var itemCount = callBack.columns["text"].Count;
+        var newDialogue = callBack.columns["text"][itemCount - 1].value;
+        print(newDialogue);
+
+        if (!_currentDialogueText.Equals(newDialogue))
+        {
+            _currentDialogueText = newDialogue;
+            TextParser.UpdateText(_currentDialogueText);
+        }
+    }
+
+
+
+
+}
---
+++ Sans Talking Machine/Assets/Scripts/TextParser.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextParser : MonoBehaviour
+{
+    public Text DialogueBox;
+    public AudioSource AudioSource;
+    public AudioClip SE;
+    public float UpdateInterval;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void UpdateText(string text)
+    {
+        DialogueBox.text = string.Empty;
+        StartCoroutine("TextUpdateCoroutine", text);
+    }
+
+    private IEnumerator TextUpdateCoroutine(string text)
+    {
+        for(var i =0;i < text.Length; i++)
+        {
+            if (!text[i].Equals(' ')) AudioSource.PlayOneShot(SE);
+            
+            DialogueBox.text+=text[i];
+
+            yield return new WaitForSeconds(UpdateInterval);
+        }
+
+    }
+}
